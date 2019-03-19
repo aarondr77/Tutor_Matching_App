ee20ebf031ffde1a30b48fc9e96a3fb117576118
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2019-03-19 18:16:43 -0400

    added Session read/write methods

--- app/src/main/java/com/example/a350project/DataManagement.java
+++ app/src/main/java/com/example/a350project/DataManagement.java
@@ -25,8 +25,53 @@ public class DataManagement {
 
     }
 
-    public void searchTutoringSessions () {
+    public static List<SessionObject> searchTutoringSessions (String subject) {
+        String FILENAME = "Sessions.txt";
+        //String string = newComplaint.getContent() + "," + newComplaint.getSubmitter() + "," + newComplaint.getStatus() + "," + newComplaint.getTarget();
 
+        BufferedReader fos = null;
+
+        List<SessionObject> tempList = new LinkedList<SessionObject>();
+        List<SessionObject> returnVal = new LinkedList<SessionObject>();
+
+        try {
+            fos = new BufferedReader(new InputStreamReader(MainActivity.context.openFileInput(FILENAME)));
+
+            while (fos.ready()) {
+                String curLine = fos.readLine();
+                Log.d("READ VALUE", curLine.split(":")[0]);
+                tempList.add(new SessionObject(curLine.split(":")[0], curLine.split(":")[1], curLine.split(":")[2], curLine.split(":")[3],
+                        curLine.split(":")[4], curLine.split(":")[5], curLine.split(":")[6]));
+            }
+            fos.close();
+
+            for (SessionObject currentSession : tempList) {
+                if (currentSession.getSubject().equals(subject)) {
+                    returnVal.add(currentSession);
+                }
+            }
+
+            return returnVal;
+        } catch (IOException e) {
+            Log.d("PRINT", e.toString());
+        }
+
+        return null;
+    }
+
+    public static void writeSession(Context context, SessionObject newSession) {
+        String FILENAME = "Session.txt";
+        String sessionString = newSession.getTutor() + ":" + newSession.getStudent() + ":" + newSession.getSubject() + ":" +
+                newSession.getDate() + ":" + newSession.getDuration() + ":" + newSession.getPrice() + ":" + newSession.getStatus();
+
+        BufferedWriter fos = null;
+        try {
+            fos = new BufferedWriter( new OutputStreamWriter(context.openFileOutput(FILENAME, Context.MODE_APPEND)));
+            fos.write(sessionString);
+            fos.close();
+        } catch (IOException e) {
+            Log.d("PRINT", e.toString());
+        }
     }
 
 
