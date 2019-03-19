789564243e91931ae00bfdc6e65940967104b245
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2019-03-19 12:08:14 -0400

    started DataManagement

--- app/src/main/java/com/example/a350project/DataManagement.java
+++ app/src/main/java/com/example/a350project/DataManagement.java
@@ -2,5 +2,16 @@ package com.example.a350project;
 
 public class DataManagement {
 
+    String sessionDatabase;
+
+    public DataManagement(String sessionDatabase) {
+
+    }
+
+    public void searchTutoringSessions () {
+
+    }
+
+
 
 }
