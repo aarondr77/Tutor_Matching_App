087ebd193d7532db991c2f68a7173f583d3a97a5
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2019-03-19 18:01:29 -0400

    created session object

---
+++ app/src/main/java/com/example/a350project/SessionObject.java
@@ -0,0 +1,58 @@
+package com.example.a350project;
+
+public class SessionObject {
+
+    private String tutor;
+    private String student;
+    private String subject;
+    private String date;
+    private String duration;    // minutes
+    private String price;       // dollars
+    private String status;
+
+    public SessionObject (String tutor, String student,
+        String subject, String date, String duration, String price, String status) {
+
+        this.tutor = tutor;
+        this.student = student;
+        this.subject = subject;
+        this.date = date;
+        this.duration = duration;
+        this.price = price;
+        this.status = status;
+    }
+
+    public String getTutor () {
+        return this.tutor;
+    }
+
+    public String getStudent () {
+        return this.student;
+    }
+
+    public String getSubject () {
+        return this.subject;
+    }
+
+    public String getDate () {
+        return this.date;
+    }
+
+    public String getDuration () {
+        return this.duration;
+    }
+
+    public String getPrice () {
+        return this.price;
+    }
+
+    public String getStatus () {
+        return this.status;
+    }
+
+
+
+
+
+
+}
