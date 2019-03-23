8ab23959fd95fde33875621737343e30f68734dc
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2019-03-23 16:24:11 -0400

    fixed bugs

--- app/src/main/java/com/example/a350project/DataManagement.java
+++ app/src/main/java/com/example/a350project/DataManagement.java
@@ -16,8 +16,8 @@ import org.json.JSONException;
 
 public class DataManagement {
 
-    private static String sessionDatabase = "Sessions36.txt";
-    private static String userDatabase = "UserDatabase36.txt";
+    private static String sessionDatabase = "Sessions37.txt";
+    private static String userDatabase = "UserDatabase37.txt";
     private static String complaintsDatabase = "ComplaintsFile.txt";
 
     public DataManagement() { }
