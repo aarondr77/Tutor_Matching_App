23b70bcfcc7cd5ed4718f6750e5e028bdc178eb7
Petra Robertson <petrar@seas.upenn.edu>
2019-04-03 16:38:16 -0400

    adding database file

---
+++ person-app/models/database.js
@@ -0,0 +1,11 @@
+// import the User class from User.js
+var User = require('./models/User.js');
+
+
+var getUsersAndQualifications = function() {
+	
+}
+
+var database = {
+	get_users_and_qualifications: getUsersAndQualifications,
+};
